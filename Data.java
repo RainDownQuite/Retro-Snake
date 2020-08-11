7001017c3135a52e559c58f09d3d40c562d6520c
RainDownQuite <69497689+RainDownQuite@users.noreply.github.com>
2020-08-11 13:14:29 +0800

    Create Data.java
    导入位于包tu的图片

---
+++ Data.java
@@ -0,0 +1,61 @@
+package tcs;
+
+import java.net.URL;
+import java.sql.Date;
+import javax.swing.*;
+
+public class Data {
+	public static URL shangURL=Data.class.getResource("/tu/shang.png");
+	public static ImageIcon shang=new ImageIcon(shangURL);
+	
+	public static URL xiaURL=Data.class.getResource("/tu/xia.png");
+	public static ImageIcon xia=new ImageIcon(xiaURL);
+	
+	public static URL zuoURL=Data.class.getResource("/tu/zuo.png");
+	public static ImageIcon zuo=new ImageIcon(zuoURL);
+	
+	public static URL youURL=Data.class.getResource("/tu/you.png");  
+	public static ImageIcon you=new ImageIcon(youURL);
+	
+	public static URL shengtiURL =Date.class.getResource("/tu/shengti.png");
+	public static ImageIcon shengti=new ImageIcon(shengtiURL);
+	
+	public static URL shiwuURL =Data.class.getResource("/tu/shiwu.png");
+	public static ImageIcon shiwu=new ImageIcon(shiwuURL);
+	
+	public static URL NEURL =Date.class.getResource("/tu/NE.png");
+	public static ImageIcon NE=new ImageIcon(NEURL);
+	
+	public static URL NWURL =Date.class.getResource("/tu/NW.png");
+	public static ImageIcon NW=new ImageIcon(NWURL);
+	
+	public static URL SEURL =Date.class.getResource("/tu/SE.png");
+	public static ImageIcon SE=new ImageIcon(SEURL);
+	
+	public static URL SWURL =Date.class.getResource("/tu/SW.png");
+	public static ImageIcon SW=new ImageIcon(SWURL);
+	
+	public static URL NS1URL =Date.class.getResource("/tu/NS1.png");
+	public static ImageIcon NS1=new ImageIcon(NS1URL);
+	
+	public static URL NS2URL =Date.class.getResource("/tu/NS2.png");
+	public static ImageIcon NS2=new ImageIcon(NS2URL);
+	
+	public static URL EW1URL =Date.class.getResource("/tu/EW1.png");
+	public static ImageIcon EW1=new ImageIcon(EW1URL);
+	
+	public static URL EW2URL =Date.class.getResource("/tu/EW2.png");
+	public static ImageIcon EW2=new ImageIcon(EW2URL);
+	
+	public static URL WEINURL =Date.class.getResource("/tu/WEIN.png");
+	public static ImageIcon WEIN=new ImageIcon(WEINURL);
+	
+	public static URL WEIEURL =Date.class.getResource("/tu/WEIE.png");
+	public static ImageIcon WEIE=new ImageIcon(WEIEURL);
+	
+	public static URL WEISURL =Date.class.getResource("/tu/WEIS.png");
+	public static ImageIcon WEIS=new ImageIcon(WEISURL);
+	
+	public static URL WEIWURL =Date.class.getResource("/tu/WEIW.png");
+	public static ImageIcon WEIW=new ImageIcon(WEIWURL);
+}
